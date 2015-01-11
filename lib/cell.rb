f82f8b4c14f223fea8c7394422167d313dd19a22
Nestor Castillo <nestor.castillo@izea.com>
2015-01-11 14:38:39 -0500

    Added alive? and dead? methods to cell.

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,8 +1,19 @@
 class Cell
   attr_reader :x, :y
+  attr_accessor :alive
 
-  def initialize(x, y)
+  def initialize(x, y, alive=true)
     @x = x
     @y = y
+    @alive = alive
   end
+
+  def alive?
+    @alive
+  end
+
+  def dead?
+    !@alive
+  end
+
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -8,5 +8,31 @@ require './lib/cell'
 # Any dead cell with exactly three live neighbours becomes
 # a live cell, as if by reproduction.
 describe Cell do
+  context "#alive?" do
+    it "defaults to true when initialized" do
+      cell = Cell.new(0,0)
 
+      expect(cell.alive?).to eq true
+    end
+
+    it "returns false when dead" do
+      cell = Cell.new(0,0,false)
+
+      expect(cell.alive?).to eq false
+    end
+  end
+
+  context "#dead?" do
+    it "returns true when dead" do
+      cell = Cell.new(0,0,false)
+
+      expect(cell.dead?).to eq true
+    end
+
+    it "return false when alive" do
+      cell = Cell.new(0,0,true)
+
+      expect(cell.dead?).to eq false
+    end
+  end
 end
